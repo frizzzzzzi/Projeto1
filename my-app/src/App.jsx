38a0c3d6e831954092e4821d8eef779a69ada98d
Fabio Rizzi <Fabio.rizzi@gmail.com>
2024-12-28 08:57:06 -0300

    Feddback parte final

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,16 +9,35 @@ const Button = (props) => {
 }
 
 const Statistics = (props) => {
+  if (props.good+props.bad+props.neutral>0) {
+    return (
+      <div>
+      <table>
+        <tbody>
+        <StatisticLine text="Good: " value={props.good} />
+        <StatisticLine text="Neutral: " value={props.neutral} />
+        <StatisticLine text="Bad: " value={props.bad} />
+        <StatisticLine text="All: " value={props.good + props.neutral + props.bad} />
+        <StatisticLine text="Average: " value={((props.good - props.bad) / (props.good + props.neutral + props.bad)).toFixed(2)} />
+        <StatisticLine text="Positive: " value={`${((props.good / (props.good + props.neutral + props.bad)) * 100).toFixed(0)} %`} />
+        </tbody>
+      </table>
+      </div>
+    )
+  }
   return (
     <div>
-      <h1>Statistics</h1>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.good+props.neutral+props.bad}</p>
-      <p>Average: {(props.good-props.bad)/(props.good+props.neutral+props.bad)}</p>
-      <p>Positive: {((props.good)/(props.good+props.neutral+props.bad))}</p>  
+      <p>No feedback given</p>
     </div>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -34,6 +53,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text='Good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral' />  
       <Button handleClick={() => setBad(bad + 1)} text='Bad' />
+      <h1>Stastistics</h1>  
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
