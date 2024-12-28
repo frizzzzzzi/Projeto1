a2822d74cfd06a68be1da2007e81e41a68694dd3
Fabio Rizzi <Fabio.rizzi@gmail.com>
2024-12-28 08:28:45 -0300

    Feedback parte 2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -23,7 +23,10 @@ const App = () => {
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>  
+      <p>Bad: {bad}</p>
+      <p>All: {good+neutral+bad}</p>
+      <p>Average: {(good-bad)/(good+neutral+bad)}</p>
+      <p>Positive: {((good)/(good+neutral+bad))}</p>  
     </div>
   )
 }
